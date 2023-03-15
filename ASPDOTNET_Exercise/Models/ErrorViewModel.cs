5dfd0422ca0e8ca5bc8e8a58d40c2b1d8ec3970b
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-03-14 21:00:58 -0400

    done

---
+++ ASPDOTNET_Exercise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPDOTNET_Exercise.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
